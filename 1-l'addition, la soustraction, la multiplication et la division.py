2bbe6d255ab20eaa2cf2154ebc756e47768f2d9a
Taher Sghaier <78413769+tahesghaier@users.noreply.github.com>
2022-10-22 14:05:56 +0100

    Project Python

---
+++ 1-l'addition, la soustraction, la multiplication et la division.py
@@ -0,0 +1,12 @@
+#L'addition 
+a=1+1
+print('La somme de 1+1 est egale a : ',a)
+#La soustraction 
+b=2-1
+print('La soustraction de 2-1 est egale a : ',b)
+#La multiplication 
+c=2*4
+print('La multiplication de 2*4 est egale a : ',c)
+#La division 
+d=8/2
+print('La division de 8/2 est egale a : ',d)
---
+++ 2-Paire et impaire.py
@@ -0,0 +1,5 @@
+n=int(input('Donner un entier: '))
+if (n%2==0) :
+    print(n,'est paire')
+else :
+    print(n,'est impaire')
